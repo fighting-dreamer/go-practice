35899bc26d3ddf94ece66cca3ad63fda9146915f
nipun.jindal <mansapatiala@gmail.com>
2024-01-28 12:53:57 +0530

    dsa graph adjency list

---
+++ dsa/graph_traversals.go
@@ -0,0 +1,67 @@
+package main
+
+import "fmt"
+
+type Node[T any] struct {
+	Id int
+	Wt T
+}
+
+type Graph[T any] struct {
+	V   int
+	E   int
+	adj [][]*Node[T]
+}
+
+func NewGraph[T any](v int, e int) *Graph[T] {
+	return &Graph[T]{
+		V:   v,
+		E:   e,
+		adj: make([][]*Node[T], v),
+	}
+}
+
+func (g *Graph[T]) AddEdge(u, v int, wt T) {
+	if u < 0 || u >= g.V || v < 0 || v >= g.V {
+		return
+	}
+	g.adj[u] = append(g.adj[u], &Node[T]{
+		Id: v,
+		Wt: wt,
+	})
+	g.adj[v] = append(g.adj[v], &Node[T]{
+		Id: u,
+		Wt: wt,
+	})
+}
+
+func (g *Graph[T]) AddDirectedEdge(u, v int, wt T) {
+	if u < 0 || u >= g.V || v < 0 || v >= g.V {
+		return
+	}
+	g.adj[u] = append(g.adj[u], &Node[T]{
+		Id: v,
+		Wt: wt,
+	})
+}
+
+func (g *Graph[T]) printGraphAdjList() {
+	for i := 0; i < g.V; i++ {
+		for j := 0; j < len(g.adj[i]); j++ {
+			n := g.adj[i][j]
+			fmt.Println(i, n.Id, n.Wt)
+		}
+	}
+}
+
+func main() {
+	g := NewGraph[int](5, 6)
+	g.AddEdge(0, 1, 1)
+	g.AddEdge(0, 4, 1)
+	g.AddEdge(1, 3, 1)
+	g.AddEdge(1, 2, 1)
+	g.AddEdge(2, 3, 1)
+	g.AddEdge(3, 4, 1)
+
+	g.printGraphAdjList()
+}
