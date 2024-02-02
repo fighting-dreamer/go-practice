a780313b2c4dcdd2903a7576c3969b69bd2e0a10
nipun.jindal <mansapatiala@gmail.com>
2024-02-02 16:58:58 +0530

    dsa binary search floor end ceil of lement

--- dsa/binary_search/floor_and_ceil_of_element.go
+++ dsa/binary_search/floor_and_ceil_of_element.go
@@ -52,16 +52,42 @@ func floor_of_element(nums []int, n, t int) int {
 		mid := (start + end) / 2
 		if nums[mid] < t {
 			res = nums[mid]
+			// if there is a valid next element and it is less than t => keep searching in right => start = mid + 1 and continue
+			//else => we have found the floor.
 			if mid+1 != n && nums[mid+1] < t {
 				start = mid + 1
 				continue
 			}
 			return res
 		} else {
+			// if equal or greater, we want to search in left of the array.
 			end = mid - 1
 		}
 	}
 
+	return -1
+}
+
+func ceil_of_element(nums []int, n, t int) int {
+	// ceil => smallest of all the larger elements thatn t
+	// find a smaller element that t say res, if there exist an element to just left that is larger than t => search in left continue
+	start := 0
+	end := n - 1
+	res := nums[0]
+
+	for start <= end {
+		mid := (start + end) / 2
+		if nums[mid] > t {
+			res = nums[mid]
+			if mid != 0 && nums[mid-1] > t {
+				end = mid - 1
+				continue
+			}
+			return res
+		} else {
+			start = mid + 1
+		}
+	}
 	return -1
 }
 
@@ -74,4 +100,5 @@ func main() {
 	}
 
 	fmt.Println(floor_of_element(nums, n, t))
+	fmt.Println(ceil_of_element(nums, n, t))
 }
