f1cc0efb67a020ee37757b0a088aa3b6e8eeadb7
nipun.jindal <mansapatiala@gmail.com>
2024-02-24 10:50:30 +0530

    random zigzag traversal

---
+++ random/zigzag_binary_tree_traversal.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+type Node struct {
+	Val   int
+	Left  *Node
+	Right *Node
+}
+
+func readInt() int {
+	var n int
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		log.Fatal("could not read input number", err)
+	}
+	return n
+}
+
+func readString() string {
+	var s string
+	_, err := fmt.Scan(&s)
+	if err != nil {
+		log.Fatal("could not read input string", err)
+	}
+	return s
+}
+
+func readDouble() float64 {
+	var f float64
+	_, err := fmt.Scanf("%f", &f)
+	if err != nil {
+		log.Fatal("could not read input float", err)
+	}
+	return f
+}
+
+func readChar() rune {
+	var r rune
+	_, err := fmt.Scanf("%c", &r)
+	if err != nil {
+		log.Fatal("Could not read input char", err)
+	}
+	return r
+}
+
+func zigzagTraversal(head *Node) []int {
+	return nil
+}
+
+func main() {
+
+}
