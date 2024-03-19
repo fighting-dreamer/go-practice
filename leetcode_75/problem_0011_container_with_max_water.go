6e5a1c908fbee78c1d0a48d29c1878c134663481
nipun.jindal <mansapatiala@gmail.com>
2024-03-19 12:16:29 +0530

    leetcode_75/problem_0011_container_with_max_water

---
+++ leetcode_75/problem_0011_container_with_max_water.go
@@ -0,0 +1,19 @@
+package main
+
+func maxArea(height []int) int {
+	n := len(height)
+	l, r := 0, n-1
+	res := -1
+	for l < r {
+		area := (r - l) * min(height[r], height[l])
+		res = max(res, area)
+
+		if height[l] < height[r] {
+			l++
+		}else {
+			r--
+		}
+	}
+
+	return res
+}
