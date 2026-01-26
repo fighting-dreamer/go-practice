e33d317217e233d0fef85c05d34457d931ef95a4
Nipun Jindal <mansapatiala@gmail.com>
2026-01-26 18:54:28 +0530

    338

---
+++ 2026/01_neetcode_150/338_counting_bits.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func readInt() int {
+	var n int
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		log.Fatal("could not read input number", err)
+	}
+	return n
+}
+
+func readString() string {
+	var s string
+	_, err := fmt.Scan(&s)
+	if err != nil {
+		log.Fatal("could not read input string", err)
+	}
+	return s
+}
+
+func readDouble() float64 {
+	var f float64
+	_, err := fmt.Scanf("%f", &f)
+	if err != nil {
+		log.Fatal("could not read input float", err)
+	}
+	return f
+}
+
+func readChar() rune {
+	var r rune
+	_, err := fmt.Scanf("%c", &r)
+	if err != nil {
+		log.Fatal("Could not read input char", err)
+	}
+	return r
+}
+
+func count_1s(n int) int {
+	cnt := 0
+	for n != 0 {
+		n = n & (n - 1)
+		cnt++
+	}
+
+	return cnt
+}
+
+func main() {
+	n := readInt()
+	for i := 0; i < n; i++ {
+		fmt.Println(i, " => ", count_1s(i))
+	}
+}
