4eb670121173699e5ee0f7bb4059623a66629315
Nipun Jindal <mansapatiala@gmail.com>
2026-01-27 23:41:48 +0530

    54

---
+++ 2026/01_neetcode_150/54_spiral_matrix.go
@@ -0,0 +1,6 @@
+package main
+
+// spiral matrix : doable
+// you traverse for n-1 elements on first row, then last columns and last row backwards and first column upwards
+// you keep track of inner row sizes and the indexes for start and ending of matrix row nad columns
+// make it recursive going inwards.
