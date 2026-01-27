f77c36352cfe53c6d1230f821a8fa5be6e5a62dd
Nipun Jindal <mansapatiala@gmail.com>
2026-01-27 17:38:16 +0530

    40

---
+++ 2026/01_neetcode_150/40_combination_sum_2.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func readInt() int {
+	var n int
+	_, err := fmt.Scanf("%d", &n)
+	if err != nil {
+		log.Fatal("could not read input number", err)
+	}
+	return n
+}
+
+func readString() string {
+	var s string
+	_, err := fmt.Scan(&s)
+	if err != nil {
+		log.Fatal("could not read input string", err)
+	}
+	return s
+}
+
+func readDouble() float64 {
+	var f float64
+	_, err := fmt.Scanf("%f", &f)
+	if err != nil {
+		log.Fatal("could not read input float", err)
+	}
+	return f
+}
+
+func readChar() rune {
+	var r rune
+	_, err := fmt.Scanf("%c", &r)
+	if err != nil {
+		log.Fatal("Could not read input char", err)
+	}
+	return r
+}
+
+func backtracking(arr []int, n int, target int, curr int, sumTillNow int, path []int, res *[][]int) {
+	if sumTillNow == target {
+		*res = append(*res, path)
+		return
+	}
+
+	for i := curr + 1; i < n; i++ {
+		if sumTillNow+arr[i] <= target {
+			backtracking(arr, n, target, i, sumTillNow+arr[i], append(path, arr[i]), res)
+		}
+	}
+}
+
+func combination_sum_2(arr []int, n int, target int) [][]int {
+	res := [][]int{}
+	backtracking(arr, n, target, 0, 0, []int{}, &res)
+	for i := 0; i < len(res); i++ {
+		fmt.Println(res[i])
+	}
+	return res
+}
+
+func main() {
+	n := readInt()
+	arr := make([]int, n)
+	for i := 0; i < n; i++ {
+		arr[i] = readInt()
+	}
+	target := readInt()
+
+	combination_sum_2(arr, n, target)
+}
