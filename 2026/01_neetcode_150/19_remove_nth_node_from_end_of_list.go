d51f4265da8b3a4f9be48c0eb56a2489b2ff1e4b
Nipun Jindal <mansapatiala@gmail.com>
2026-01-27 15:12:13 +0530

    19

---
+++ 2026/01_neetcode_150/19_remove_nth_node_from_end_of_list.go
@@ -0,0 +1,6 @@
+package main
+
+// remove nth node from end of list
+// two points, first one already n steps ahead, if possible, as if list length is > n
+// both pointer then start moving at same pace, when first one reaches the end => the second one is at nth node
+// remove the second node.
