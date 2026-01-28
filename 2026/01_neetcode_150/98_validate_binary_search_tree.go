cd665cf1f5a091f5522ab05eb0cb57b9beca1aa4
Nipun Jindal <mansapatiala@gmail.com>
2026-01-28 15:48:35 +0530

    98

---
+++ 2026/01_neetcode_150/98_validate_binary_search_tree.go
@@ -0,0 +1,5 @@
+package main
+
+// validate binary search tree : keep min and max value possible at each node
+// for root node, it is INT_MIN and INT_MAX respectively
+// each node value must lie in between corresponding min and max value for that node.
